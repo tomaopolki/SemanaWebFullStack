707e65f383d9434ed3a37c3d771234ff7e47f484
Felipe Tomao <fetomao@hotmail.com>
2020-11-27 14:14:56 -0300

    db changes

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,6 +3,6 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(`mysql://root:${process.env.MYSQL_PASS}@localhost:3306/pitu`);
+const sequelize = new Sequelize(`mysql://root:${process.env.MYSQL_PASS}@localhost:3306/ziguen`);
 
 export default sequelize;
